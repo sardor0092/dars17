d337bb19bc5de8cf181b053af1966b34f6b9b9d0
sardor0092 <sardorkhaydarov099@gmail.com>
2024-12-06 20:26:51 +0500

    birinchi yuklash

---
+++ mian.js
@@ -0,0 +1,44 @@
+// Maqsad: Javascripga kirish va asosiy tushunchalari.
+
+// Kirish: 
+// 1. Javascript nima?
+// Web -sahifalar interaktiv qilish uchun ishlatiladigan dasturlash tili.
+// Front-end va backend dasturlashda ishlatilishi mumkin.
+
+
+// Qayerda ishlaydi?
+// Brauzer (Chrome , Firefox , Opera) va node.js serverlarda.
+
+
+// console.log("salom sardor");
+
+// Yil	ECMA	Brauzer
+// 1995 yil	 	JavaScript Brendan Eich tomonidan ixtiro qilingan
+// 1996 yil	 	Netscape 2 JavaScript 1.0 bilan chiqarildi
+// 1997 yil	 	JavaScript ECMA standartiga aylandi (ECMA-262)
+// 1997 yil	ES1	ECMAScript 1 chiqarildi
+// 1997 yil	ES1	IE 4 ES1 ni qo'llab-quvvatlaydigan birinchi brauzer edi
+// 1998 yil	ES2	ECMAScript 2 chiqarildi
+// 1998 yil	 	Netscape 42 JavaScript 1.3 bilan chiqarildi
+// 1999 yil	ES2	IE 5 ES2-ni qo'llab-quvvatlaydigan birinchi brauzer edi
+// 1999 yil	ES3	ECMAScript 3 chiqarildi
+// 2000	ES3	IE 5.5 ES3-ni qo'llab-quvvatlaydigan birinchi brauzer edi
+// 2000	 	Netscape 62 JavaScript 1.5 bilan chiqarilgan
+// 2000	 	Firefox 1 JavaScript 1.5 bilan chiqarildi
+// 2008 yil	ES4	ECMAScript 4 tark etildi
+// 2009 yil	ES5	ECMAScript 5 chiqarildi
+// 2011 yil	ES5	IE 9 ES5 * ni qo'llab-quvvatlaydigan birinchi brauzer edi
+// 2011 yil	ES5	Firefox 4 JavaScript 1.8.5 bilan chiqarildi
+// 2012 yil	ES5	Safari 6-da ES5-ni to'liq qo'llab-quvvatlash
+// 2012 yil	ES5	IE 10 da ES5 uchun to'liq qo'llab-quvvatlash
+// 2012 yil	ES5	Chrome 23-da ES5-ni to'liq qo'llab-quvvatlash
+// 2013 yil	ES5	Firefox 21 da ES5 uchun to'liq qo'llab-quvvatlash
+// 2013 yil	ES5	Opera 15 da ES5 uchun to'liq qo'llab-quvvatlash
+// 2014 yil	ES5	Barcha brauzerlarda ES5 uchun to'liq qo'llab-quvvatlash
+// 2015 yil	ES6	ECMAScript 6 chiqarildi
+// 2016 yil	ES6	Chrome 51-da ES6-ni to'liq qo'llab-quvvatlash
+// 2016 yil	ES6	Opera 38-da ES6-ni to'liq qo'llab-quvvatlash
+// 2016 yil	ES6	Safari 10-da ES6-ni to'liq qo'llab-quvvatlash
+// 2017 yil	ES6	Firefox 54 da ES6 uchun to'liq qo'llab-quvvatlash
+// 2017 yil	ES6	Edge 15-da ES6-ni to'liq qo'llab-quvvatlash
+// 2018	ES6	Barcha brauzerlarda ES6-ni to'liq qo'llab-quvvatlash **
